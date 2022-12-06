12c24ebc6802a48b9c47a153f8b2164508429a30
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-12-06 22:43:56 +0200

    Formik and yup implemented

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -1,16 +1,37 @@
-import { Form, FormLabel, FormInput, FormBtn } from './ContactForm.styled';
+import {
+  SearchForm,
+  FormLabel,
+  FormInput,
+  FormBtn,
+} from './ContactForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContacts, addMyContact } from 'redux/contactsSlice';
 import { nanoid } from 'nanoid';
+import { Formik, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object({
+  name: yup
+    .string()
+    .matches(/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/)
+    .min(2, 'Too short!')
+    .max(30, 'Too long!')
+    .required('Required'),
+  number: yup
+    .string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/
+    )
+    .min(5, 'Too short!')
+    .max(30, 'Too long!')
+    .required('Required'),
+});
 
 export default function ContactForm() {
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    const name = event.currentTarget.elements.name.value;
-    const number = event.currentTarget.elements.number.value;
+  const handleSubmit = ({ name, number }, { resetForm }) => {
     const isExist = contacts.find(contact => contact.name === name);
 
     if (isExist) {
@@ -25,29 +46,36 @@ export default function ContactForm() {
     };
 
     dispatch(addMyContact(contact));
-
-    document.getElementById('form').reset();
+    resetForm();
   };
 
   return (
-    <Form onSubmit={handleSubmit} id="form">
-      <FormLabel>Name</FormLabel>
-      <FormInput
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-      <FormLabel>Number</FormLabel>
-      <FormInput
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-      />
-      <FormBtn type="submit">Add contact</FormBtn>
-    </Form>
+    <Formik
+      initialValues={{ name: '', number: '' }}
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+    >
+      <SearchForm id="form">
+        <FormLabel>Name</FormLabel>
+        <ErrorMessage name="name" />
+        <FormInput
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <FormLabel>Number</FormLabel>
+        <ErrorMessage name="number" />
+        <FormInput
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+        <FormBtn type="submit">Add contact</FormBtn>
+      </SearchForm>
+    </Formik>
   );
 }
